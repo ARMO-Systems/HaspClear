ae716c30bd682d24e0b1ab04da4b7cf1d3a92822
Podlesnyy Andrey <podlesniy@armo.ru>
2014-06-25 12:50:40 +0400

    Log haspclear

--- Source/HaspClearClient/Program.cs
+++ Source/HaspClearClient/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using ArmoSystems.ArmoGet.HaspClearClient.HaspClearServiceReference;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace ArmoSystems.ArmoGet.HaspClearClient
 {
@@ -8,16 +11,39 @@ namespace ArmoSystems.ArmoGet.HaspClearClient
         [STAThread]
         private static int Main()
         {
+            SetupLog();
+            var logger = LogManager.GetLogger( "WcfService" );
             try
             {
+                logger.Info( "Вызываю сервис" );
                 new ServiceClient().RestartSLM( Environment.MachineName );
+                logger.Info( "Успешно сброшены лицензии" );
                 return 0;
             }
             catch ( Exception ex )
             {
+                logger.Info( ex.ToString() );
                 Console.WriteLine( ex.ToString() );
                 return -1;
             }
+        }
+
+        private static void SetupLog()
+        {
+            var config = new LoggingConfiguration();
+
+            var fileTarget = new FileTarget();
+            config.AddTarget( "file", fileTarget );
+
+            // Step 3. Set target properties 
+            fileTarget.FileName = @"D:\Temp\HaspClearService.txt";
+            fileTarget.Layout = "${longdate}|${message}";
+
+            var rule = new LoggingRule( "*", LogLevel.Debug, fileTarget );
+            config.LoggingRules.Add( rule );
+
+            // Step 5. Activate the configuration
+            LogManager.Configuration = config;
         }
     }
 }
